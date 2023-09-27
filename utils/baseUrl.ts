c1448d9a87201c11d3fe8f9343dc3bebf01fa5c7
khafui <manianthonio@gmail.com>
2023-09-27 23:55:56 +0000

    fix test 1

--- utils/baseUrl.ts
+++ utils/baseUrl.ts
@@ -1,5 +1,5 @@
 export const getBaseUrl = () => {
   return process.env.NODE_ENV === 'development'
     ? process.env.NEXT_PUBLIC_BASE_URL_DEVELOPMENT
-    : process.env.NEXT_PUBLIC_BASE_URL_PRODUCTION;
+    : process.env.VERCEL_URL;
 };
